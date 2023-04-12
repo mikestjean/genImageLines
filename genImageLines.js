3937218914ed654d7b3f08dfd9e36393b72b96fc
mikestjean <mstjean@gmail.com>
2023-04-11 22:45:06 -0400

    Update genImageLines.js

--- genImageLines.js
+++ genImageLines.js
@@ -12,26 +12,7 @@ img1.onload = () => {
   // Create a copy of the original image data
   const originalImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-  let time = 0;
-  function animate() {
-    time += 0.1;
-    for (let i = 0; i < numLines; i++) {
-      const y = i * lineHeight;
-      const alpha = (Math.sin(time + y * 0.01) + 1) / 2; // generate alpha value from sin wave
-      const imageData = ctx.createImageData(canvas.width, lineHeight);
-      // Use the original image data as the source for each line
-      imageData.data.set(originalImageData.data.subarray(y * canvas.width * 4, (y + lineHeight) * canvas.width * 4));
-      // Set alpha value for each pixel in the line
-      for (let j = 3; j < imageData.data.length; j += 4) {
-        imageData.data[j] *= alpha;
-      }
-      ctx.putImageData(imageData, 0, y);
-    }
-    requestAnimationFrame(animate);
-  }
-  animate();
-
-  // Load the second image
+  let time2 = 0;
   const img2 = new Image();
   img2.onload = () => {
     // Create a new canvas element to hold the blended result
@@ -40,8 +21,25 @@ img1.onload = () => {
     blendCanvas.height = canvas.height;
     const blendCtx = blendCanvas.getContext('2d');
 
-    blendCtx.globalCompositeOperation = 'source-over';
-    blendCtx.drawImage(img2, 0, 0);
+    function animate2() {
+      time2 += 0.1;
+      for (let i = 0; i < numLines; i++) {
+        const y = i * lineHeight;
+        const alpha2 = (Math.sin(time2 + y * 0.01) + 1) / 2; // generate alpha value from sin wave
+        const imageData2 = blendCtx.createImageData(canvas.width, lineHeight);
+        // Use the original image data as the source for each line
+        imageData2.data.set(originalImageData.data.subarray(y * canvas.width * 4, (y + lineHeight) * canvas.width * 4));
+        // Set alpha value for each pixel in the line
+        for (let j = 3; j < imageData2.data.length; j += 4) {
+          imageData2.data[j] *= alpha2;
+        }
+        blendCtx.putImageData(imageData2, 0, y);
+      }
+      blendCtx.globalCompositeOperation = 'destination-over';
+      blendCtx.drawImage(img2, 0, 0);
+      requestAnimationFrame(animate2);
+    }
+    animate2();
 
     // Display the blended result
     document.body.appendChild(blendCanvas);
