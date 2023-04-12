3243e1d299ea12fed7218db9c80450c2ec7f2bbe
mikestjean <mstjean@gmail.com>
2023-04-11 21:33:13 -0400

    Update genImageLines.js

--- genImageLines.js
+++ genImageLines.js
@@ -5,14 +5,13 @@ img1.onload = () => {
   canvas.height = img1.height;
   const ctx = canvas.getContext('2d');
   ctx.drawImage(img1, 0, 0);
-  document.body.appendChild(canvas);
 
   const numLines = 1280;
   const lineHeight = canvas.height / numLines;
 
   // Create a copy of the original image data
   const originalImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  
+
   let time = 0;
   function animate() {
     time += 0.1;
@@ -32,16 +31,14 @@ img1.onload = () => {
   }
   animate();
 
-  // Add second image on top of the first one
+  // Load the second image
   const img2 = new Image();
   img2.onload = () => {
-    const canvas2 = document.createElement('canvas');
-    canvas2.width = img2.width;
-    canvas2.height = img2.height;
-    const ctx2 = canvas2.getContext('2d');
-    ctx2.drawImage(img2, 0, 0);
-    
-    const originalImageData2 = ctx2.getImageData(0, 0, canvas2.width, canvas2.height);
+    // Create a new canvas element to hold the blended result
+    const blendCanvas = document.createElement('canvas');
+    blendCanvas.width = canvas.width;
+    blendCanvas.height = canvas.height;
+    const blendCtx = blendCanvas.getContext('2d');
 
     let time2 = 0;
     function animate2() {
@@ -49,32 +46,20 @@ img1.onload = () => {
       for (let i = 0; i < numLines; i++) {
         const y = i * lineHeight;
         const alpha2 = (Math.sin(time2 + y * 0.01) + 1) / 2; // generate alpha value from sin wave
-        const imageData2 = ctx2.createImageData(canvas2.width, lineHeight);
+        const imageData2 = blendCtx.createImageData(canvas.width, lineHeight);
         // Use the original image data as the source for each line
-        imageData2.data.set(originalImageData2.data.subarray(y * canvas2.width * 4, (y + lineHeight) * canvas2.width * 4));
+        imageData2.data.set(originalImageData.data.subarray(y * canvas.width * 4, (y + lineHeight) * canvas.width * 4));
         // Set alpha value for each pixel in the line
         for (let j = 3; j < imageData2.data.length; j += 4) {
-          imageData2.data[j] *= (1 - alpha2);
+          imageData2.data[j] *= alpha2;
         }
-        ctx2.putImageData(imageData2, 0, y);
+        blendCtx.putImageData(imageData2, 0, y);
       }
+      blendCtx.globalCompositeOperation = 'multiply';
+      blendCtx.drawImage(img2, 0, 0);
       requestAnimationFrame(animate2);
     }
     animate2();
-
-    // Create a new canvas element to hold the blended result
-    const blendCanvas = document.createElement('canvas');
-    blendCanvas.width = canvas.width;
-    blendCanvas.height = canvas.height;
-    const blendCtx = blendCanvas.getContext('2d');
-      blendCtx.globalCompositeOperation = 'multiply';
-    
-
-     // Draw the first image onto the blended canvas
-    blendCtx.drawImage(img1, 0, 0);
-
-    // Draw the second image onto the blended canvas
-    blendCtx.drawImage(canvas, 0, 0);
 
     // Display the blended result
     document.body.appendChild(blendCanvas);
