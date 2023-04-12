5f2058cf9a86ced60b4fa7ac245a53e4e4975706
mikestjean <mstjean@gmail.com>
2023-04-11 21:04:05 -0400

    Update genImageLines.js
    Changed Composite Operation to 'source-over'

--- genImageLines.js
+++ genImageLines.js
@@ -20,7 +20,6 @@ img1.onload = () => {
     for (let i = 0; i < numLines; i++) {
       const y = i * lineHeight;
       const alpha = (Math.sin(time + y * 0.01) + 1) / 2; // generate alpha value from sin wave
-      console.log(alpha);
       const imageData = ctx.createImageData(canvas.width, lineHeight);
       // Use the original image data as the source for each line
       imageData.data.set(originalImageData.data.subarray(y * canvas.width * 4, (y + lineHeight) * canvas.width * 4));
@@ -38,7 +37,7 @@ img1.onload = () => {
   const img2 = new Image();
   img2.onload = () => {
     const ctx2 = canvas.getContext('2d');
-    ctx2.globalCompositeOperation = 'difference';
+    ctx2.globalCompositeOperation = 'source-over';
     ctx2.drawImage(img2, 0, 0);
     
     const originalImageData2 = ctx2.getImageData(0, 0, canvas.width, canvas.height);
@@ -49,7 +48,6 @@ img1.onload = () => {
       for (let i = 0; i < numLines; i++) {
         const y = i * lineHeight;
         const alpha2 = (Math.sin(time2 + y * 0.01) + 1) / 2; // generate alpha value from sin wave
-        console.log("alpha2" + alpha2);
         const imageData2 = ctx2.createImageData(canvas.width, lineHeight);
         // Use the original image data as the source for each line
         imageData2.data.set(originalImageData2.data.subarray(y * canvas.width * 4, (y + lineHeight) * canvas.width * 4));
